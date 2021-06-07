36212d56534b395ad7bed6796dbd677a9eab99ab
jczarfeld <83969720+jczarfeld@users.noreply.github.com>
2021-06-07 12:49:02 -0400

    Add files via upload

---
+++ PREWORK_JHO/Module-6/javascript3.js
@@ -0,0 +1,28 @@
+$(document).ready(
+    
+document.getElementById("Blue").addEventListener("click", function Blue(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+}))
+$(document).ready(
+document.getElementById("Grow").addEventListener("click", function Grow(){
+
+    document.getElementById("box").style.height = "250px";
+}))
+$(document).ready(
+
+document.getElementById("Fade").addEventListener("click", function Fade(){
+
+    document.getElementById("box").style.opacity = "0.5";
+}))
+$(document).ready(
+
+document.getElementById("Reset").addEventListener("click", function Reset(){
+
+    document.getElementById("box").style.height = "150px";
+}))
+$(document).ready(
+document.getElementById("Reset").addEventListener("click", function Reset(){
+
+    document.getElementById("box").style.opacity = "1";
+}))
